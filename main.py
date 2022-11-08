a88809fd15914ec57d2fd67fd7634c8075fb92a0
virtualu-mag <huanmle38@gmail.com>
2022-11-07 20:02:09 -0500

    song loading for a user is complete

--- main.py
+++ main.py
@@ -10,29 +10,33 @@ import urllib
 
 sg.theme('DarkAmber')
 
+
 class User:
-  def __init__(self, name, country, pp, globalRank, localRank, rankedAcc, rankedCount):
-    self.name = name
-    self.country = country
-    self.pp = pp
-    self.globalRank = globalRank
-    self.localRank = localRank
-    self.rankedAcc = rankedAcc
-    self.rankedCount = rankedCount
+    def __init__(self, name, country, pp, globalRank, localRank, rankedAcc, rankedCount):
+        self.name = name
+        self.country = country
+        self.pp = pp
+        self.globalRank = globalRank
+        self.localRank = localRank
+        self.rankedAcc = rankedAcc
+        self.rankedCount = rankedCount
+
 
 class Song:
-  def __init__(self, name, artist, mapper, score, accuracy, pp, img, fullCombo, maxCombo, badCuts, misses):
-    self.name = name
-    self.artist = artist
-    self.mapper = mapper
-    self.score = score
-    self.accuracy = accuracy
-    self.pp = pp
-    self.image = img
-    self.fullCombo = fullCombo
-    self.maxCombo = maxCombo
-    self.badCuts = badCuts
-    self.misses = misses
+    def __init__(self, name, artist, mapper, stars, score, accuracy, pp, img, fullCombo, maxCombo, badCuts, misses):
+        self.name = name
+        self.artist = artist
+        self.mapper = mapper
+        self.stars = stars
+        self.score = score
+        self.accuracy = accuracy
+        self.pp = pp
+        self.image = img
+        self.fullCombo = fullCombo
+        self.maxCombo = maxCombo
+        self.badCuts = badCuts
+        self.misses = misses
+
 
 def loadUser(userID):
     user_response = requests.get('https://scoresaber.com/api/player/' + str(userID) + '/full')
@@ -48,7 +52,6 @@ def loadUser(userID):
     return newUser
 
 
-
 def LoadUserSongs(userID, len):
     # play_response = requests.get('https://scoresaber.com/api/player/' + str(userID) + '/scores?limit=' + str(99) + 'sort=top&withMetadata=true')
     play_response = requests.get(
@@ -56,11 +59,23 @@ def LoadUserSongs(userID, len):
     songList = []
 
     for x in range(len):
-        print(play_response.json()['playerScores'][x]['leaderboard']['songName'])
-        print(play_response.json()['playerScores'][x]['leaderboard']['songAuthorName'])
-        print(play_response.json()['playerScores'][x]['leaderboard']['levelAuthorName'])
+        songList.append(Song(play_response.json()['playerScores'][x]['leaderboard']['songName'],
+                             play_response.json()['playerScores'][x]['leaderboard']['songAuthorName'],
+                             play_response.json()['playerScores'][x]['leaderboard']['levelAuthorName'],
+                             play_response.json()['playerScores'][x]['leaderboard']['stars'],
+                             play_response.json()['playerScores'][x]['score']['baseScore'],
+                             100 * (play_response.json()['playerScores'][x]['score']['baseScore'] / play_response.json()['playerScores'][x]['leaderboard']['maxScore']),
+                             play_response.json()['playerScores'][x]['score']['pp'],
+                             play_response.json()['playerScores'][x]['leaderboard']['coverImage'],
+                             play_response.json()['playerScores'][x]['score']['fullCombo'],
+                             play_response.json()['playerScores'][x]['score']['maxCombo'],
+                             play_response.json()['playerScores'][x]['score']['badCuts'],
+                             play_response.json()['playerScores'][x]['score']['missedNotes']
+                             )
+                        )
 
     return songList
+
 
 def getImage(img_url):
     jpg_data = (
@@ -75,6 +90,7 @@ def getImage(img_url):
     png_bio = io.BytesIO()
     pil_image.save(png_bio, format="PNG")
     return png_bio.getvalue()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
