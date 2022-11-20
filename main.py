f16d7627feef8147034b6f3b3a74a9234c79945a
agogte <agogte@gmu.edu>
2022-11-20 13:12:36 -0500

    Plot working

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# from plot_graph import plot_graph
 import tkinter
 import requests
 import json
@@ -8,6 +9,9 @@ import PySimpleGUI as sg
 import cloudscraper
 import urllib
 import datetime
+import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 sg.theme('DarkAmber')
 
@@ -122,7 +126,69 @@ def SortTest(SongList):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    User1 = loadUser(76561198002500746)
+    # User1 = loadUser(76561198002500746)
     SongList = LoadUserSongs(76561198002500746, 100)
-    SortTest(SongList.copy())
+    unweightedPP = []
+    for i in range(100):
+        unweightedPP.append(SongList[i].pp)
+    # print(unweightedPP)
+    # SortTest(SongList.copy())
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# plot function is created for 
+# plotting the graph in 
+# tkinter window
+def plot_graph(y):
+  
+    # the figure that will contain the plot
+    fig = Figure(figsize=(5,5), dpi=100)
+
+  
+    # adding the subplot
+    plot1 = fig.add_subplot(111)
+    plot1.grid()
+    plot1.set_xlabel('Play counts')
+    plot1.set_ylabel('Unweighted PP')
+  
+    # plotting the graph
+    plot1.plot(y)
+  
+    # creating the Tkinter canvas
+    # containing the Matplotlib figure
+    canvas = FigureCanvasTkAgg(fig,
+                               master = window)  
+    canvas.draw()
+  
+    # placing the canvas on the Tkinter window
+    canvas.get_tk_widget().pack()
+  
+    # creating the Matplotlib toolbar
+    toolbar = NavigationToolbar2Tk(canvas,window)
+    toolbar.update()
+  
+    # placing the toolbar on the Tkinter window
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+  
+# the main Tkinter window
+window = tk.Tk()
+  
+# setting the title 
+window.title('UnweightedPP vs play count')
+  
+# dimensions of the main window
+window.geometry("500x500")
+  
+# button that displays the plot
+plot_button = tk.Button(master = window, 
+                     command = plot_graph(y = unweightedPP),
+                     height = 2, 
+                     width = 10,
+                     text = "Plot")
+  
+# place the button 
+# in main window
+plot_button.pack()
+  
+# run the gui
+window.mainloop()
