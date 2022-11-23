a402828b16b4f32f570ff250d2904198cb7a1794
virtualu-mag <huanmle38@gmail.com>
2022-11-23 13:00:42 -0500

    base main window with user panels rendering properly

--- Player/PlayerFrame.py
+++ Player/PlayerFrame.py
@@ -6,9 +6,9 @@ import requests
 import re
 from urlextract import URLExtract
 
-from PlayerPanel.ViewPanelFrame import ViewPlayer
-from EditPanel.EditPanelFrame import EditPlayer
-from RecentPanel.RecentPanelFrame import RecentPlayer
+from .PlayerPanel.ViewPanelFrame import ViewPlayer
+from .EditPanel.EditPanelFrame import EditPlayer
+from .RecentPanel.RecentPanelFrame import RecentPlayer
 
 import user
 
@@ -32,6 +32,7 @@ class PlayerWidget(Frame):
 
         self.current_window.tkraise()
 
+
     def navigate(self, name):
         # Hide all screens
         for window in self.windows.values():
@@ -40,25 +41,25 @@ class PlayerWidget(Frame):
         # Show the screen of the button pressed
         self.windows[name].place(x=0, y=0, width=540.0, height=240.0)
 
-if __name__ == "__main__":
-    test = Tk()
-
-    # User1 = loadUser(76561198002500746)  # Acerola
-    # Player1 = loadUser(76561198404774259) #SilentBang
-    # Player2 = loadUser(76561198333869741) #Cerret
-
-    # Player1 = loadUser('https://scoresaber.com/u/761198002500746')
-    Player1 = user.loadUser('https://scoresaber.com/u/76561198002500746')
-    Player2 = user.loadUser('76561198002500746')
-
-    test.geometry("1100x300")
-    test.configure(bg="#343638")
-
-    PlayerFrame1 = PlayerWidget(Player1, test, bg="#343638", width=540, height=240)
-    PlayerFrame1.place(x=0, y=0, width=540.0, height=240.0)
-
-    PlayerFrame2 = PlayerWidget(Player2, test, bg="#343638", width=540, height=240)
-    PlayerFrame2.place(x=540, y=0, width=540.0, height=240.0)
-
-    test.resizable(False, False)
-    test.mainloop()
+# if __name__ == "__main__":
+#     test = Tk()
+#
+#     # User1 = loadUser(76561198002500746)  # Acerola
+#     # Player1 = loadUser(76561198404774259) #SilentBang
+#     # Player2 = loadUser(76561198333869741) #Cerret
+#
+#     # Player1 = loadUser('https://scoresaber.com/u/761198002500746')
+#     Player1 = user.loadUser('https://scoresaber.com/u/76561198002500746')
+#     Player2 = user.loadUser('76561198002500746')
+#
+#     test.geometry("1100x300")
+#     test.configure(bg="#343638")
+#
+#     PlayerFrame1 = PlayerWidget(Player1, test, bg="#343638", width=540, height=240)
+#     PlayerFrame1.place(x=0, y=0, width=540.0, height=240.0)
+#
+#     PlayerFrame2 = PlayerWidget(Player2, test, bg="#343638", width=540, height=240)
+#     PlayerFrame2.place(x=540, y=0, width=540.0, height=240.0)
+#
+#     test.resizable(False, False)
+#     test.mainloop()
