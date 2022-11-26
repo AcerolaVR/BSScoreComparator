37b2aa439213916e1d8db721d65642d9a5423b35
virtualu-mag <huanmle38@gmail.com>
2022-11-26 02:19:49 -0500

    loads into json file, but will add duplicates

--- user.py
+++ user.py
@@ -4,7 +4,7 @@ import re
 import tkinter as tk
 import traceback
 import tkinter.messagebox
-
+import json
 
 class User:
     def __init__(self, id, name, country, pp, globalRank, localRank, rankedAcc, rankedCount, icon):
@@ -52,6 +52,8 @@ def loadUser(userInput):
     if (user_response.status_code == 404):
         raise Exception(error)
 
+    write_json(user_response.json())
+
     newUser = User(user_response.json()["id"],
                    user_response.json()["name"],
                    user_response.json()["country"],
@@ -63,6 +65,19 @@ def loadUser(userInput):
                    user_response.json()['profilePicture'])
 
     return newUser
+
+
+# function to add to JSON
+def write_json(new_data, filename='recentUsers.json'):
+    with open(filename, 'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data["recent_players"].append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent=4)
 
 
 def report_callback_exception(self, *args):
