0e9c0fbddbba85c3edac2f24d6387618329999a8
virtualu-mag <huanmle38@gmail.com>
2022-11-26 20:23:28 -0500

    leaderboard + graph frame added to main window

---
+++ LeaderGraph/LeaderGraphFrame.py
@@ -0,0 +1,39 @@
+import tkinter as tk  # python 3
+from tkinter import font as tkfont  # python 3
+from tkinter import Tk, Canvas, Listbox, Entry, Text, Button, PhotoImage, END, Frame
+
+import requests
+import re
+from urlextract import URLExtract
+
+from .SongPanel.ViewSongPanel import ViewSong
+
+import user
+
+class LeaderGraphWidget(Frame):
+    def __init__(self, parent, controller=None, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        # self.player = player
+
+        self.configure(bg="#343638")
+
+        # Loop through windows and place them
+        self.windows = {
+            "leaderboard": ViewSong(self),
+            "graph": ViewSong(self),
+        }
+
+        self.current_window = self.windows["leaderboard"]
+        self.navigate("leaderboard")
+
+        self.current_window.tkraise()
+
+
+    def navigate(self, name):
+        # Hide all screens
+        for window in self.windows.values():
+            window.place_forget()
+
+        # Show the screen of the button pressed
+        self.windows[name].place(x=0, y=0, width=1113.0, height=680.0)
